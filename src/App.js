839bed9ce69717f368155dd0f2049d0bdea3d1e5
EricPippert67 <pipperte@gmail.com>
2020-04-03 15:35:44 -0600

    last commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
+import CurrencyConverter from './Components/CurrencyConverter'
+import CurrencyDisplay from './Components/CurrencyDisplay'
 import './App.css';
+
 
 class App extends Component {
   render() {
     return (
-      <div>
-
-      </div>
-    );
+      <>
+				<h2>Render Props</h2>
+        <CurrencyConverter 
+              render={(currencyData, amount)=> (
+                <p>
+                  US Dollar ${amount.toFixed(2)} - {currencyData.name}{' '}
+                  {currencyData.symbol}
+                  {(amount * currencyData.rate).toFixed(2)}
+                </p>
+              )}
+           />
+			</>
+    )
   }
 }
 
